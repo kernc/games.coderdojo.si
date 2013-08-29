7ad179efb3a291a406670303b979f6193c3e104d
Kernc <kerncece@gmail.com>
2013-08-29 10:57:09 +0200

    initial commit of site

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  hide_all();
+  var current = $(window.location.hash.replace('/', '\\/'));
+  if (current.length)
+    current.parent().show();
+  else game('#about');
+  
+  var external_links = $('#nav a[href^="http"]');
+  external_links.css('background-image', function (index, value) {
+    return "url('images/new-window.png'), " + value;
+  });
+  external_links.attr('target', '_blank');
+  
+  $('#nav a[href^="#"]').click(function() {
+    game($(this).attr('href'));
+    window.submenu = $(this).parents('.submenu');
+    window.submenu.addClass('submenu-hide').removeClass('submenu');
+    setTimeout(function() {
+      window.submenu.addClass('submenu').removeClass('submenu-hide');
+    }, 500);
+  });
+});
+function hide_all() { $('section.game').hide(); }
+
+function game(id) {
+  hide_all();
+  $(id.replace('/', '\\/')).parent().show();
+  window.location.assign(id);
+}
+
+function embedSWF (swf, id, height) {
+  height = height || 500;
+  swfobject.embedSWF(swf, id, 800, height, '11.0.0', false, {}, {wmode:'opaque', bgcolor:'#eeeeee', base:'flash/'}, {});
+}
+
+window.onhashchange = function() {
+  game(window.location.hash);
+}
